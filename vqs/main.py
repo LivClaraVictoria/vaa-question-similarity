784e01ec41d2d19085a3ebea791d8dc6aee4374b
LivClaraVictoria <livweiss95@gmail.com>
2025-10-03 11:55:27 +0200

    set up simple experiment

--- vqs/main.py
+++ vqs/main.py
@@ -1,23 +1,44 @@
+import pandas as pd
+from pathlib import Path
+from itertools import combinations
 from sentence_transformers import SentenceTransformer
 
 # 1. Load a pretrained Sentence Transformer model
-model = SentenceTransformer("all-MiniLM-L6-v2")
+SBERT = SentenceTransformer("all-MiniLM-L6-v2")  # just some basic SBERT encoder
 
-# The sentences to encode
-sentences = [
-    "The weather is lovely today.",
-    "It's so sunny outside!",
-    "He drove to the stadium.",
-]
+# CHANGE! --> no hardcoded paths
+# df = pd.read_csv(
+#     "C:/Users/liv/git/thesis/vaa-question-similarity/data/raw/smart vote data/smartvote_2019_NR_Questions.csv"
+# )
 
-# 2. Calculate embeddings by calling model.encode()
-embeddings = model.encode(sentences)
-print(embeddings.shape)
-# [3, 384]
+# CHANGE! paths --> diff file/look up path handling
+root = Path(__file__).parent.parent
+data_df_path = root / "data" / "raw" / "smart vote data" / "df_Questions_2019.pk1"
+experiment_path = root / "experiments" / "similarities.csv"
 
-# 3. Calculate the embedding similarities
-similarities = model.similarity(embeddings, embeddings)
-print(similarities)
-# tensor([[1.0000, 0.6660, 0.1046],
-#         [0.6660, 1.0000, 0.1411],
-#         [0.1046, 0.1411, 1.0000]])
+# make list of questions
+df = pd.read_pickle(data_df_path)
+questions = df["question_en"].tolist()
+
+embeddings = SBERT.encode(questions)
+
+
+similarities = SBERT.similarity(embeddings, embeddings)
+results = []
+for i, j in combinations(range(len(questions)), 2):
+    results.append(
+        {"Qu1": questions[i], "Qu2": questions[j], "Similarity": similarities[i][j]}
+    )
+
+df_results = pd.DataFrame(results)
+df_results.sort_values(by="Similarity", ascending=False, inplace=True)
+df_results.to_csv(experiment_path, index=False)
+
+# # 2. Calculate embeddings by calling model.encode()
+# embeddings = SBERT.encode(sentences)
+# print(embeddings.shape)
+# # [3, 384]
+
+# # 3. Calculate the embedding similarities
+# similarities = SBERT.similarity(embeddings, embeddings)
+# print(similarities)
