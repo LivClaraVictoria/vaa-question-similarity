27497b1f9017e7fdb8a8f8f352bb8082cd2b2018
LivClaraVictoria <livweiss95@gmail.com>
2026-02-21 17:12:51 +0100

    updated bug in cloned configs

--- configs/full_pipeline/cloned/base_cloned.py
+++ configs/full_pipeline/cloned/base_cloned.py
@@ -1,12 +1,7 @@
 from configs.base_constants import *
 
-CLEANED_DIR = CLONED_DIR / clone_id
-QUESTIONS_2023_PATH = CLEANED_DIR / "df_questions.parquet"
-QUESTIONS_2019_PATH = CLEANED_DIR / "df_questions19.parquet"
-RAW_VOTERS_2023_PATH = CLEANED_DIR / "df_voters_topmatch.parquet"
 
 data_choice = "cloned"
-clone_id = "identical_q32214_n10"
 
 load_candidates = True
 load_voters = True
